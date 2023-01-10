b50531f438f77553731bf0e54bb1255d2753083b
nibirrayy <nibirrayy@gmail.com>
2023-01-10 13:48:50 +0530

    CORS handling added

--- app.js
+++ app.js
@@ -3,6 +3,19 @@ const app = express();
 
 const port = 3000;
 
+// Modifying the headers of all responses to allow CORS (Cross-Origin-Resource-Sharing)
+app.use((req, res) => {
+  res.header("Access-Control-Allow-Origin", " * ");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authotization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT,POST,PATCH,DELTE,GET");
+    return res.status(200).json({});
+  }
+});
+
 app.get("/", (req, res) => {
   res.send("Hello world");
 });
